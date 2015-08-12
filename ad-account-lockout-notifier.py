6c6daa6ccca6d6cbe34362b1cf231dd8a1c3a5c5
williamstlr <williams.tlr@gmail.com>
2015-08-12 17:59:25 -0600

    Error Messages
    Started working on some error messages that output to the Event Viewer.

--- ad-account-lockout-notifier.py
+++ ad-account-lockout-notifier.py
@@ -17,19 +17,31 @@ import smtplib
 sender = 'vs-w2k8-pdc@myhorizoncu.com'
 receivers = ['tyler@myhorizoncu.com','8013098557@message.ting.com']
 subject = "AD Account Lockout"
-server = "mail.myhorizoncu.com"
+server = "zmail.myhorizoncu.com"
 
 #The windows event for an AD lockout is 4740, sometimes this will be 4672 for testing
-windowsEvent = subprocess.check_output('wevtutil qe Security "/q:*[System [(EventID=4672)]]" /f:text /rd:true /c:1')
-searchString = re.search('Account Name:.*',windowsEvent)
-lockedUser = (searchString.group(0).split())[2]
-emailMessage = "The following AD account has been locked: %s" %(lockedUser)
+try:
+	windowsEvent = subprocess.check_output('wevtutil qe Security "/q:*[System [(EventID=4672)]]" /f:text /rd:true /c:1')
+	searchString = re.search('Account Name:.*',windowsEvent)
+	lockedUser = (searchString.group(0).split())[2]
+	emailMessage = "The following AD account has been locked: %s" %(lockedUser)
 
-#Setup email server and send the email
-emailServer = smtplib.SMTP(server)
-emailServer.sendmail(sender, receivers, emailMessage)
-print "Successfully sent email"
-emailServer.quit()
+except:
+	subprocess.call('eventcreate /ID 1 /L APPLICATION /T WARNING  /SO AD-Lockout-Notifier /D "Lockout notification failed to find and parse a username."')
+	exit(1)
+	
+	
+try:
+	#Setup email server and send the email
+	emailServer = smtplib.SMTP(server)
+	emailServer.sendmail(sender, receivers, emailMessage)
+	subprocess.call('eventcreate /ID 1 /L APPLICATION /T INFORMATION  /SO AD-Lockout-Notifier /D "Notification emails were sent successfully."')
+	emailServer.quit()
+
+except:
+	subprocess.call('eventcreate /ID 1 /L APPLICATION /T WARNING  /SO AD-Lockout-Notifier /D "Lockout notification failed to send notification emails."')
+	exit(1)
+
 
 exit(0)
 

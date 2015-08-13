fb3848b9a497834984aacc1ffc459e958ffa0c70
williamstlr <williams.tlr@gmail.com>
2015-08-13 16:53:23 -0600

    py2exe builds
    Runs with py2exe now. It is compiled in the dist directory.

--- ad-account-lockout-notifier.py
+++ ad-account-lockout-notifier.py
@@ -11,24 +11,25 @@
 #################################################
 import subprocess
 import re
-import smtplib
+import smtplib,socket
+import sys
 
 #Email setup
 sender = 'vs-w2k8-pdc@myhorizoncu.com'
 receivers = ['tyler@myhorizoncu.com','8013098557@message.ting.com']
 subject = "AD Account Lockout"
-server = "zmail.myhorizoncu.com"
+server = "mail.myhorizoncu.com"
 
 #The windows event for an AD lockout is 4740, sometimes this will be 4672 for testing
 try:
-	windowsEvent = subprocess.check_output('wevtutil qe Security "/q:*[System [(EventID=4672)]]" /f:text /rd:true /c:1')
+	windowsEvent = subprocess.check_output('wevtutil qe Security "/q:*[System [(EventID=4740)]]" /f:text /rd:true /c:1')
 	searchString = re.search('Account Name:.*',windowsEvent)
 	lockedUser = (searchString.group(0).split())[2]
 	emailMessage = "The following AD account has been locked: %s" %(lockedUser)
 
 except:
-	subprocess.call('eventcreate /ID 1 /L APPLICATION /T WARNING  /SO AD-Lockout-Notifier /D "Lockout notification failed to find and parse a username."')
-	exit(1)
+	subprocess.call('eventcreate /ID 1 /L APPLICATION /T WARNING  /SO AD-Lockout-Notifier /D "Could not query the Windows Event Log."')
+	sys.exit()
 	
 	
 try:
@@ -38,10 +39,10 @@ try:
 	subprocess.call('eventcreate /ID 1 /L APPLICATION /T INFORMATION  /SO AD-Lockout-Notifier /D "Notification emails were sent successfully."')
 	emailServer.quit()
 
-except:
-	subprocess.call('eventcreate /ID 1 /L APPLICATION /T WARNING  /SO AD-Lockout-Notifier /D "Lockout notification failed to send notification emails."')
-	exit(1)
+except socket.error:
+	subprocess.call('eventcreate /ID 1 /L APPLICATION /T WARNING  /SO AD-Lockout-Notifier /D "Could not connect to server to send mail."')
+	sys.exit(1)
 
 
-exit(0)
+sys.exit(0)
 
---
+++ setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=['ad-account-lockout-notifier.py'])
